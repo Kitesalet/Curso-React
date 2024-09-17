951dda229d3c9aebe1d622665c7a331a0716edb9
Administrator <Administrator@DESKTOP-QKT5I78>
2024-09-17 18:49:06 -0300

    first commit

---
+++ Ejercicio Curso 1/src/App.jsx
@@ -0,0 +1,39 @@
+import GenericInput from "./GenericInput";
+import GenericButton from "./GenericButton";
+import { useState } from "react";
+
+function App() {
+  function onSubmit(e) {
+    e.preventDefault();
+
+    alert("Couldn't log in because this is incomplete ;(");
+  }
+
+  const inputConstructor = [
+    { name: "Nombre", inputType: "text" },
+    { name: "Apellido", inputType: "text" },
+    { name: "Email", inputType: "email" },
+    { name: "Phone Number", inputType: "number" },
+    { name: "Password", inputType: "password" },
+    { name: "Confirm Password", inputType: "password" },
+  ];
+
+  return (
+    <div className="container">
+      <form onSubmit={(e) => onSubmit(e)} className="container form">
+        <h1>LOGIN</h1>
+        {inputConstructor.map((ic) => {
+          return (
+            <GenericInput
+              name={ic.name}
+              inputType={ic.inputType}
+            ></GenericInput>
+          );
+        })}
+        <GenericButton text={"SUBMIT"}></GenericButton>
+      </form>
+    </div>
+  );
+}
+
+export default App;
